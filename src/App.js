035bd875478f65f079403ef74a14175c8b1dbc3a
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-04-18 13:35:09 +0530

    test completed & app.jsmodified inline with results

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-// import { Route } from "react-router-dom";
 
 import {    Route,Routes } from "react-router-dom";
 import Welcome from "./components/Welcome";
-// import Product from "./components/Product";
 import MainHeader from "./components/MainHeader";
 import Login from "./Login";
 import Register from "./Register";
 
-// import { useNavigate } from 'react-router';
 
  
 
@@ -21,34 +18,11 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <MainHeader /> */}
 
-{/* <Login /> */}
-
-{/* <Register /> */}
-
-{/* <Welcome /> */}
-
-{/* <MainHeader /> */}
-{/* <Login /> */}
-
-        {/* <p>Hello, Hello!</p> */}
         <header>
           <MainHeader />
          </header>
-{/* <h2>Welcome to Our Page</h2> */}
         <main>
-        {/* <Routes> */}
-        
-          {/* <Route path="/login">
-          <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route> */}
 
 <Routes>
   <Route path="/login" element={<Login />} />
@@ -56,7 +30,6 @@ export default class App extends React.Component {
   <Route path="/welcome" element={<Welcome />} />
 </Routes>
 
-          {/* </Routes > */}
 
         </main>
     
