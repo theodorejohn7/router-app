7cc864002d292c503d1ce9ca4d9bdfbf9ebbfcf1
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-04-18 13:20:25 +0530

    product.js comment removed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,9 @@
-//  import React from 'react';
- import React, { Component }  from 'react';
+ import React   from 'react';
 
 
 function  Product   ()   {
     return <h1> The Product Page</h1>;
 
-};
+}
 
 export default Product;
